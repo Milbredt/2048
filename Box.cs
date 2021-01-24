04dab1d62346bf48c23d67b5f71c9fa6a0abd06a
Filip Milbredt <filip_milbredt@hotmail.com>
2021-01-24 12:16:00 +0100

    Added neighbours List<int>

--- Box.cs
+++ Box.cs
@@ -1,10 +1,14 @@
-class Box
+using System.Collections.Generic;
+
+public class Box
 {
     public int BoxNumber {get;}
     public int NumberInBox {get; set;}
     public bool Changed {get; set;}
 
     public bool New {get; set;} //Används för att se om det är en automatiskt tillagd ruta och då sätta annan färg på den vid utskrift.
+
+    public List<int> neighbours = new List<int>();
 
     public Box(int boxNumber)
     {
